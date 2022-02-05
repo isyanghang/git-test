4c4572f9e5cd87ee4a0df1be734967f4fe68808e
yh <isyanghang@163.com>
2022-02-05 10:34:29 +0800

    first commit

---
+++ src/main/java/com/yh/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.yh.git;
+
+/**
+ * @author yhstart
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
